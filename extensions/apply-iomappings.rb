34e7e46a9a6435e79203758ea1cb86bcae9b3a46
Max F. Albrecht <1@178.is>
2017-01-29 18:50:07 +0100

    maintenance: iomapping to terms

--- extensions/apply-iomappings.rb
+++ extensions/apply-iomappings.rb
@@ -3,7 +3,8 @@
 # NOTE: this assumes that the files itself have not changed.
 # thus it's relatively fast because the files metadata is saved in DB.
 
-maps = IoMapping.where(io_interface_id: 'file_meta_data_mappings')
+# maps = IoMapping.where(io_interface_id: 'file_meta_data_mappings')
+maps = IoMapping.all
 mapped_keys = maps.map(&:key_map).uniq
 
 # for file metadata of all media entries,
@@ -13,14 +14,35 @@ MediaEntry.all.each do |e|
       # map meta key,
       mk = MetaKey.find_by(id: m.meta_key_id)
       next unless mk
+
       # don't overwrite any exiting data,
       next if MetaDatum.find_by(meta_key: mk, media_entry: e)
-      # create new metadatum with mapped text value
-      MetaDatum::Text.create!(
-        string: v,
-        created_by_id: e.media_file.uploader_id,
+
+      # create new metadatum with mapped value(s)
+      md_attr = {
+        created_by: e.media_file.uploader,
         meta_key: mk,
-        media_entry: e)
-    end.compact
+        media_entry: e
+      }
+
+      if m.key_map_type != 'MetaDatum::Keywords'
+        MetaDatum::Text.create!(md_attr.merge(string: v))
+      else
+        terms = if v.is_a?(Enumerable) then v
+        else
+          v.to_s.split(',').map(&:strip)
+        end
+
+        kws = terms.map do |term|
+          term = Keyword.find_or_create_by!(term: term, meta_key: mk)
+        end
+        mdkw = MetaDatum::Keywords.new(md_attr.merge(keywords: kws))
+        mdkw.meta_data_keywords.each do |mdk|
+          mdk.created_by = e.media_file.uploader
+        end
+        mdkw.save!
+      end
+
+    end
   end
 end
--- extensions/file-metadata.rb
+++ extensions/file-metadata.rb
@@ -10,16 +10,19 @@ data = <<-YAML
 
   - id: iptc_city
     title: City
+    type: keyword
     mappings:
     - IPTC:City
 
   - id: province_state
     title: Province/State
+    type: keyword
     mappings:
     - IPTC:Province-State
 
   - id: iptc_core_country_code
     title: Country Code
+    type: keyword
     mappings:
       - XMP-iptcCore:CountryCode
       - IPTC:Country-PrimaryLocationCode
@@ -36,36 +39,43 @@ data = <<-YAML
 
   - id: exif_aperture_value
     title: Camera Aperture Value
+    type: keyword
     mappings:
     - ExifIFD:ApertureValue
 
   - id: exif_focal_length
     title: Camera Focal Length
     description: Focal Length (in 35mm format)
+    type: keyword
     mappings:
     - ExifIFD:FocalLengthIn35mmFormat
 
   - id: exif_exposure_time
     title: Camera Exposure Time
+    type: keyword
     mappings:
     - ExifIFD:ExposureTime
 
   - id: exif_iso
     title: Camera ISO
+    type: keyword
     mappings:
     - ExifIFD:ISO
 
   - id: exif_lens_info
     title: Camera Lens Info
+    type: keyword
     mappings:
     - ExifIFD:LensInfo
 
   - id: exif_lens_make
+    type: keyword
     title: Camera Lens Make
     mappings:
     - ExifIFD:LensMake
 
   - id: exif_lens_model
+    type: keyword
     title: Camera Lens Model
     mappings:
     - ExifIFD:LensModel
@@ -104,6 +114,13 @@ data = <<-YAML
     title: GPS HPositioningError
     mappings:
     - GPS:GPSHPositioningError
+
+  - id: is_merged_panorama
+    title: Camera merged Panorama
+    type: keyword
+    mappings:
+    - XMP-aux:IsMergedPanorama
+
 YAML
 
 keys = YAML.safe_load data
@@ -111,22 +128,31 @@ keys = YAML.safe_load data
 v = Vocabulary.find_or_create_by!(id: 'file_meta_data')
 v.update_attributes!(
   label: 'File MetaData',
-  description: 'Read-only Metadata mapped from data embedded in the uploaded file',
+  description: 'Read-only MetaData mapped from data embedded in the uploaded file',
   enabled_for_public_view: true,
   enabled_for_public_use: false)
 
 i = IoInterface.find_or_create_by!(id: 'file_meta_data_mappings')
 
-keys.each do |k|
+keys.sort_by {|k| k['id']}.each.with_index do |k, n|
   mkid = "#{v.id}:#{k['id']}"
+  is_keyword = k['type'] == 'keyword'
   MetaKey.find_or_create_by!(
     id: mkid,
     vocabulary: v,
-    label: k['label'],
-    admin_comment: k['admin_comment'],
-    hint: "Maps from: #{k['mappings'].join(', ')}")
+    meta_datum_object_type: is_keyword ? 'MetaDatum::Keywords' : 'MetaDatum::Text')
+    .update_attributes!(
+      position: n,
+      label: k['title'],
+      admin_comment: k['admin_comment'],
+      hint: "Maps from: #{k['mappings'].join(', ')}")
 
   k['mappings'].each do |m|
-    IoMapping.find_or_create_by!(meta_key_id: mkid, key_map: m, io_interface: i)
+    IoMapping.find_or_create_by!(
+      meta_key_id: mkid,
+      key_map: m,
+      io_interface: i,
+      key_map_type: is_keyword ? 'MetaDatum::Keywords' : nil,
+    )
   end
 end
