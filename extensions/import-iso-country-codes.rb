47708bd62e86549a3e8efee2a057924bddfe4cd2
Max Albrecht <max.albrecht@zhdk.ch>
2017-04-24 20:05:11 +0200

    maintenance: import ISO country codes (Linked Data)

---
+++ extensions/import-iso-country-codes.rb
@@ -0,0 +1,40 @@
+# ansible-playbook -v -i hosts extensions/play_maintenance.yml -e 'ruby_script=./import-iso-country-codes.rb'
+
+# FIXME: no https???
+DATA_URL = 'http://download.geonames.org/export/dump/countryInfo.txt'
+BASE_URL = 'http://geonames.org/countries'
+
+# get data
+countries = `curl '#{DATA_URL}'`.chomp
+  .split("\n")
+  .reject { |line| line.start_with? '#' }
+  .map {|line| line.split "\t" }
+  .map {|fields| {code: fields[0], name: fields[4]}}
+
+# meta
+vocab = Vocabulary.find_or_create_by!(id: 'geo', label: 'Geo')
+mkey = MetaKey.find_or_create_by!(
+  id: 'geo:country',
+  vocabulary_id: 'geo',
+  label: 'Country',
+  description: 'Country by ISO Country Code',
+  hint: 'Two-letter country code from the ISO-3166 list',
+  meta_datum_object_type: 'MetaDatum::Keywords',
+  allowed_rdf_class: 'Country',
+  is_extensible_list: false,
+  is_enabled_for_media_entries: true
+)
+type = RdfClass.find_or_create_by!(id: 'Country')
+type.update_attributes!(
+  description: 'same as <http://schema.org/Country>'
+)
+
+# add countries
+countries.each do |country|
+  kw = Keyword.find_or_create_by!(term: country[:code], meta_key_id: 'geo:country')
+  kw.update_attributes!(
+    rdf_class: 'Country',
+    description: country[:name],
+    external_uri: "#{BASE_URL}/#{country[:code]}/"
+  )
+end
